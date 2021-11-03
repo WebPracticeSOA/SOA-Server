386fd06c8fa2e492fac9db6ff03bc61fba7a120d
山山 <181250019@smail.nju.edu.cn>
2021-11-03 22:12:01 +0800

    feat: 更改为读取服务器mysql数据库内容

--- src/main/DataService/DataService.java
+++ src/main/DataService/DataService.java
@@ -1,5 +1,6 @@
 package main.DataService;
 
+import com.alibaba.fastjson.JSONObject;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.internal.OSSHeaders;
@@ -12,41 +13,71 @@ import java.sql.*;
 
 import javax.jws.WebService;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @WebService(name = "DataService", endpointInterface = "main.DataService.DataServicePort")
 public class DataService implements DataServicePort {
-    private String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-    private String accessKeyId = "LTAI5tNVfKmpK9XDyvcyWX6f";
-    private String accessKeySecret = "730eX85eAqYEKPBzLTWE5EK2CosA0q";
-    private String bucketName = "internet-practice";
-    private String appendixEnd = "http://oss-cn-hangzhou.aliyuncs.com";
-    private String appendixBucket = "appendixlist";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://106.15.42.179:3303/practice?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    static final String USER = "root";
+    static final String PASS = "123456";
     @Override
     public String getAllData() {
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        ObjectListing objectListing = ossClient.listObjects(bucketName);
-        List<OSSObjectSummary> sums = objectListing.getObjectSummaries();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (OSSObjectSummary s : sums) {
+        Connection conn = null;
+        Statement stmt = null;
+        List<Map> result = new ArrayList<Map>();
+        try{
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM papers";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                Map<String, Object> line = new LinkedHashMap<String, Object>();
+                line.put("id", rs.getInt("id"));
+                line.put("title", rs.getString("title"));
+                line.put("number", rs.getString("number"));
+                line.put("category", rs.getString("category"));
+                line.put("department", rs.getString("department"));
+                line.put("release_time", rs.getDate("release_time"));
+                line.put("implement_time", rs.getDate("implement_time"));
+                line.put("grade", rs.getString("grade"));
+                line.put("interpret", rs.getString("interpret"));
+                line.put("user_id", rs.getInt("user_id"));
+                line.put("input_time", rs.getDate("input_time"));
+                line.put("content", rs.getString("text"));
+                line.put("status", rs.getInt("status"));
+                line.put("analyse_id", rs.getInt("analyse_id"));
+                result.add(line);
+            }
+            System.out.println(JSONObject.toJSONString(result));
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            System.out.println("sql exception");
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            System.out.println("exception");
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
             try{
-                stringBuilder.append("https://internet-practice.oss-cn-beijing.aliyuncs.com/").append(URLEncoder.encode(s.getKey(), "UTF-8")).append("\n");
-            } catch (UnsupportedEncodingException e){
-                e.printStackTrace();
-                return "Encoding Error";
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
             }
         }
-        ossClient = new OSSClientBuilder().build(appendixEnd, accessKeyId, accessKeySecret);
-        objectListing = ossClient.listObjects(appendixBucket);
-        sums = objectListing.getObjectSummaries();
-        for (OSSObjectSummary s : sums) {
-            try{
-                stringBuilder.append("https://appendixlist.oss-cn-hangzhou.aliyuncs.com/").append(URLEncoder.encode(s.getKey(), "UTF-8")).append("\n");
-            } catch (UnsupportedEncodingException e){
-                e.printStackTrace();
-                return "Encoding Error";
-            }
-        }
-        return stringBuilder.toString();
+        System.out.println("Goodbye!");
+        return JSONObject.toJSONString(result);
     }
 }
